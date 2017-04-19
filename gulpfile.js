2e42e55db8effeb751237b85070b647298807756
alexqin@intv.com.cn <alexqin@intv.com.cn>
2017-04-19 23:06:05 +0800

    upload code

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,29 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 browserSync.init({
-	proxy: 'localhost:5000'
+  proxy: 'localhost:5000'
 });
 
 gulp.task('sass', () => {
-    return sass('src/sass/**/*.sass')
-        .on('error', sass.logError)
-        .pipe(gulp.dest('public/css'))
-				.pipe(browserSync.stream())
-})
+  return sass('src/sass/**/*.sass')
+    .on('error', sass.logError)
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream())
+});
 
 gulp.task('babel', () => {
-	return gulp.src(['src/js/*.js', 'src/config/*.js'])
-		.pipe(plumber())
-		.pipe(sourcemaps.init())
-		.pipe(babel({
-			presets: ['env', 'es2015', 'stage-2']
-		}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('public/js'))
-		.pipe(browserSync.stream())
+  return gulp.src(['src/js/*.js', 'src/config/*.js'])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['env', 'es2015', 'stage-2']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream())
 })
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('src/js/*.js', ['babel']);
   gulp.watch('src/sass/**/*.sass', ['sass']);
 })
